6cc6363695731b2298fa62cc302027ba1bf72d81
enejepromise <enejepromise@gmail.com>
2023-08-17 12:34:35 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,15 @@
 
 /**
  * print_alphabet - print all alphabet in lower case
+ *
+ * Return:void
  */
 
 void print_alphabet(void)
 {
 	char letter;
 
-	for (letter  'a'
+	for (letter = 'a'; <= 'z'; letter++)
+	_putchar(letter);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,10 @@
-#include "main.h"
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: on success 1.
- * on error, -1 is required, and errno is set appropriately.
+ * on error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
