b234715bfb24b1de7332bc2834bd62af429b19ac
enejepromise <enejepromise@gmail.com>
2023-08-27 16:29:52 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string
- * @str: The string to be capitalized.
- * Return: A pointer to the changed string.
+ * cap_string - function that capitalizes all words of a string.
+ * @s: The string to be capitalized.
+ * Return: A pointer to the changed s
  */
-char *cap_string(char *str)
-{
-	int i = 0;
-	
-	while (str[i])
+
+char *cap_string(char *s)
+{	
+int string_count;
+
+/*scan through string */
+string_count = 0;
+while (s[string_count] != '\0')
+{/* if next character after count is char , capitalise it */
+	if (s[0] >= 97 && s[0] <= 122)
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z
-'))
-			i++;
-		if (str[i - 1] == ' ' ||
-		str[i - 1] == '\t' ||
-		str[i - 1] == '\n' ||
-		str[i - 1] == ',' ||
-		str[i - 1] == ';' ||
-		str[i - 1] == '.' ||
-		str[i - 1] == '!' ||
-		str[i - 1] == '?' ||
-		str[i - 1] == '"' ||
-		str[i - 1] == '(' ||
-		str[i - 1] == ')' ||
-		str[i - 1] == '{' ||
-		str[i - 1] == '}' ||
-		i == 0)
-			str[i] -= 32;
-		i++;
+		s[0] = s[0] - 32;
 	}
-	return (str);
+	if (s[string_count] == ' ' || s[string_count] == '\t' || s[string_count] == '\n'
+	|| s[string_count] == ',' || s[string_count] == ';' || s[string_count] == '.'
+	|| s[string_count] == '.' || s[string_count] == '!' || s[string_count] == '?'
+	|| s[string_count] == '"' || s[string_count] == '(' || s[string_count] == ')'
+	|| s[string_count] == '{' || s[string_count] == '}')
+	{
+		if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
+		{
+		s[string_count + 1] = s[string_count + 1] - 32;
+		}
+	}
+	string_count++;
+}
+return (s);
 }
