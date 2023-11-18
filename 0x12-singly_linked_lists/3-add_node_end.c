dd0f57abc66f8bacf07aa62a7556a54e86c74d9f
enejepromise <enejepromise@gmail.com>
2023-11-17 16:09:42 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,24 +1,13 @@
 #include <stdlib.h>
-
 #include <string.h>
-
 #include "lists.h"
 
-
 /**
-
  * add_node_end - adds a new node at the end of a linked list
-
  * @head: double pointer to the list_t list
-
  * @str: string to put in the new node
-
- *
-
  * Return: address of the new element, or NULL if it failed
-
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 
 {
@@ -40,33 +29,18 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (!new)
 
 		return (NULL);
-
-
 	new->str = strdup(str);
-
 	new->len = len;
-
 	new->next = NULL;
 
-
 	if (*head == NULL)
-
 	{
-
 		*head = new;
-
 		return (new);
-
 	}
 
-
 	while (temp->next)
-
 		temp = temp->next;
-
-
 	temp->next = new;
-
-
 	return (new);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,13 +2,9 @@
 
 #include "lists.h"
 
-
 /**
-
  * free_list - frees a linked list
-
  * @head: list_t list to be freed
-
  */
 
 void free_list(list_t *head)
